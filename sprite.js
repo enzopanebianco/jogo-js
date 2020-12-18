6dbc8f2ecb4f96632f4c1e6d666f6434378924ea
enzopanebianco <enzinho983@gmail.com>
2020-12-18 16:38:12 -0300

    init repo

---
+++ sprite.js
@@ -0,0 +1,13 @@
+function Sprite(x, y, largura, altura) {
+    this.x = x;
+    this.y = y;
+    this.largura = largura;
+    this.altura = altura;
+
+    this.desenha = function (xCanvas, yCanvas) {
+        ctx.drawImage(img, this.x, this.y, this.largura, this.altura, xCanvas, yCanvas, this.largura, this.altura);
+    }
+}
+let 
+bg = new Sprite(0, 0, 600, 600),
+spriteBoneco = new Sprite(618,16,87,87);
